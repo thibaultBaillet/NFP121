33d093b92e8579f60d7838854eb195611b4bfea2
thibaultBaillet <79105144+thibaultBaillet@users.noreply.github.com>
2021-02-15 18:34:47 +0100

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,29 @@
+package question3;
+
+    public class Phone extends Device implements MobileNetwork {
+        private int batteryLevel;
+
+        public Phone(){}
+
+        public Phone(int p_width, int p_height) {
+            this.setWidth(p_width);
+            this.setHeight(p_height);
+        }
+
+        public Phone(String p_operatingSystem){
+            this.setOperatingSystem(p_operatingSystem);
+        }
+
+        public boolean isTactile() {
+            return true;
+        }
+        public int getBatteryLevel() {
+            return batteryLevel;
+        }
+        public void setBatteryLevel(int batteryLevel) {
+            this.batteryLevel = batteryLevel;
+        }
+        public String networkType() {
+            return "4G";
+        }
+    }
