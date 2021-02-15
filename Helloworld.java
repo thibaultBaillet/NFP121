f14d0cd20d63f8f9b5002f1f20072a7083fea02a
thibaultBaillet <79105144+thibaultBaillet@users.noreply.github.com>
2021-02-15 18:09:04 +0100

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+package question1;
+
+public class Helloworld {
+    /**
+     * A method sayHello that return a string containing : Hello world!
+     * @return String
+     */
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
